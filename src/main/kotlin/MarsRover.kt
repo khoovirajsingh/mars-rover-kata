c610eb1658633a296e3bcd6d7387e57a57c0267a
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-09-29 11:41:33 +0100

    Refactored to parameterised test

--- src/main/kotlin/MarsRover.kt
+++ src/main/kotlin/MarsRover.kt
@@ -1,6 +1,9 @@
-
 class MarsRover(grid: Grid) {
+    var cardinal: String = "N"
+    var y: Int = 0
+    var x: Int = 0
+
     fun execute(command: String): String {
-        return "0:0:N"
+        return "$x:$y:$cardinal"
     }
 }
--- src/test/kotlin/MarsRoverShould.kt
+++ src/test/kotlin/MarsRoverShould.kt
@@ -1,12 +1,32 @@
 import org.amshove.kluent.`should be equal to`
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+private const val EMPTY_COMMAND = ""
 
 class MarsRoverShould {
-    @Test
-    fun move() {
-        val grid = Grid()
-        val marsRover = MarsRover(grid)
-        val position = marsRover.execute("")
-        position `should be equal to` "0:0:N"
+    lateinit var grid: Grid
+    lateinit var marsRover: MarsRover
+
+    @BeforeEach
+    fun setUp() {
+        grid = Grid()
+        marsRover = MarsRover(grid)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            "0, 0, N, '0:0:N'",
+            "5, 6, W, '5:6:W'"
+    )
+    fun `stay at the same position given an empty command`(x: Int, y: Int, cardinal: String, expectedPosition:String) {
+        marsRover.x = x
+        marsRover.y = y
+        marsRover.cardinal = cardinal
+
+        val actualPosition = marsRover.execute(EMPTY_COMMAND)
+
+        actualPosition `should be equal to` expectedPosition
     }
 }
