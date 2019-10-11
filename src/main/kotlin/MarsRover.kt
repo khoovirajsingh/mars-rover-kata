6950ed73246c5d3c3cc6a09185799c9402a14f05
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-10-11 20:47:30 +0100

    Refactored code to make it more readable

--- src/main/kotlin/MarsRover.kt
+++ src/main/kotlin/MarsRover.kt
@@ -1,22 +1,29 @@
+private const val NORTH = "N"
+private const val EAST = "E"
+
 class MarsRover(val grid: Grid) {
-    var cardinal: String = "N"
+    var cardinal: String = NORTH
     var y: Int = 0
     var x: Int = 0
 
     fun execute(commands: String): String {
         for (command in commands) {
-            if (cardinal == "N") moveNorth()
-            if (cardinal == "E") moveEast()
+            if (isNorthFacing()) moveNorth()
+            if (isEastFacing()) moveEast()
         }
         return formattedPosition()
     }
 
-    private fun moveEast() {
-        x++
-    }
+    private fun isNorthFacing() = cardinal == NORTH
 
     private fun moveNorth() {
         if (y.isAtEdge()) y = 0 else y++
+    }
+
+    private fun isEastFacing() = cardinal == EAST
+
+    private fun moveEast() {
+        x++
     }
 
     private fun formattedPosition() = "$x:$y:$cardinal"
