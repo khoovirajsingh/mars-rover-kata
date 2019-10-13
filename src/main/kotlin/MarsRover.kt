b26fbf3525dc9942e897b7f30823b1c298f89adf
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-10-13 08:21:38 +0100

    Moved more behaviour to position class

--- src/main/kotlin/MarsRover.kt
+++ src/main/kotlin/MarsRover.kt
@@ -1,6 +1,7 @@
 private const val MOVE = 'M'
 private const val ROTATE_RIGHT = 'R'
 private const val ROTATE_LEFT = 'L'
+
 class MarsRover(val grid: Grid, var position: Position) {
 
     fun execute(commands: String): String {
@@ -15,29 +16,13 @@ class MarsRover(val grid: Grid, var position: Position) {
     private fun isMove(command: Char) = command == MOVE
 
     private fun move() {
-        if (facing(NORTH)) moveNorth()
-        if (facing(EAST)) moveEast()
-        if (facing(SOUTH)) moveSouth()
-        if (facing(WEST)) moveWest()
+        if (facing(NORTH)) position.moveNorth()
+        if (facing(EAST)) position.moveEast()
+        if (facing(SOUTH)) position.moveSouth()
+        if (facing(WEST)) position.moveWest()
     }
 
     private fun facing(direction: String) = position.facing(direction)
-
-    private fun moveNorth() {
-        if (position.y.isOverBoundary()) position.y = 0 else position.y++
-    }
-
-    private fun moveEast() {
-        if (position.x.isOverBoundary()) position.x = 0 else position.x++
-    }
-
-    private fun moveSouth() {
-        if (position.y.isUnderBoundary()) position.y = SIZE - 1 else position.y--
-    }
-
-    private fun moveWest() {
-        if (position.x.isUnderBoundary()) position.x = SIZE - 1 else position.x--
-    }
 
     private fun isRotateRight(command: Char) = command == ROTATE_RIGHT
 
--- src/main/kotlin/Position.kt
+++ src/main/kotlin/Position.kt
@@ -6,5 +6,21 @@ const val WEST = "W"
 data class Position(var x: Int = 0, var y: Int = 0, var direction: String = NORTH) {
     fun facing(cardinal: String) = cardinal == direction
 
+    fun moveNorth() {
+        if (y.isOverBoundary()) y = 0 else y++
+    }
+
+    fun moveSouth() {
+        if (y.isUnderBoundary()) y = SIZE - 1 else y--
+    }
+
+    fun moveEast() {
+        if (x.isOverBoundary()) x = 0 else x++
+    }
+
+    fun moveWest() {
+        if (x.isUnderBoundary()) x = SIZE - 1 else x--
+    }
+
     override fun toString() = "${x}:${y}:${direction}"
 }
