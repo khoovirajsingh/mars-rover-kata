57b24da75308a680c771d1c5b6a64aac4b6a18c8
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-10-13 08:41:31 +0100

    Refactored and moved behaviour to position

--- src/main/kotlin/MarsRover.kt
+++ src/main/kotlin/MarsRover.kt
@@ -2,47 +2,20 @@ private const val MOVE = 'M'
 private const val ROTATE_RIGHT = 'R'
 private const val ROTATE_LEFT = 'L'
 
-class MarsRover(val grid: Grid, var position: Position) {
+class MarsRover(val grid: Grid, private var position: Position) {
 
     fun execute(commands: String): String {
         for (command in commands) {
-            if (isMove(command)) move()
-            if (isRotateRight(command)) rotateRight()
-            if (isRotateLeft(command)) rotateLeft()
+            if (isMove(command)) position = position.move()
+            if (isRotateRight(command)) position = position.rotateRight()
+            if (isRotateLeft(command)) position = position.rotateLeft()
         }
         return position.toString()
     }
 
     private fun isMove(command: Char) = command == MOVE
 
-    private fun move() {
-        if (facing(NORTH)) position = position.moveNorth()
-        if (facing(EAST)) position = position.moveEast()
-        if (facing(SOUTH)) position = position.moveSouth()
-        if (facing(WEST)) position = position.moveWest()
-    }
-
-    private fun facing(direction: String) = position.facing(direction)
-
     private fun isRotateRight(command: Char) = command == ROTATE_RIGHT
 
-    private fun rotateRight() {
-        when {
-            facing(NORTH) -> position.direction = EAST
-            facing(EAST) -> position.direction = SOUTH
-            facing(SOUTH) -> position.direction = WEST
-            facing(WEST) -> position.direction = NORTH
-        }
-    }
-
     private fun isRotateLeft(command: Char) = command == ROTATE_LEFT
-
-    private fun rotateLeft() {
-        when {
-            facing(NORTH) -> position.direction = WEST
-            facing(WEST) -> position.direction = SOUTH
-            facing(SOUTH) -> position.direction = EAST
-            facing(EAST) -> position.direction = NORTH
-        }
-    }
 }
--- src/main/kotlin/Position.kt
+++ src/main/kotlin/Position.kt
@@ -4,24 +4,53 @@ const val SOUTH = "S"
 const val WEST = "W"
 
 data class Position(var x: Int = 0, var y: Int = 0, var direction: String = NORTH) {
-    fun facing(cardinal: String) = cardinal == direction
 
-    fun moveNorth(): Position {
+    fun move(): Position {
+        if (facing(NORTH)) return moveNorth()
+        if (facing(EAST)) return moveEast()
+        if (facing(SOUTH)) return moveSouth()
+        if (facing(WEST)) return moveWest()
+        return Position(x, y, direction)
+    }
+
+    fun rotateRight(): Position {
+        return when {
+            facing(NORTH) -> Position(x, y, EAST)
+            facing(EAST) -> Position(x, y, SOUTH)
+            facing(SOUTH) -> Position(x, y, WEST)
+            facing(WEST) -> Position(x, y, NORTH)
+            else -> Position(x, y, direction)
+        }
+    }
+
+    fun rotateLeft(): Position {
+        return when {
+            facing(NORTH) -> Position(x, y, WEST)
+            facing(WEST) -> Position(x, y, SOUTH)
+            facing(SOUTH) -> Position(x, y, EAST)
+            facing(EAST) -> Position(x, y, NORTH)
+            else -> Position(x, y, direction)
+        }
+    }
+
+    private fun facing(cardinal: String) = cardinal == direction
+
+    private fun moveNorth(): Position {
         if (y.isOverBoundary()) y = 0 else y++
         return Position(x, y, direction)
     }
 
-    fun moveSouth(): Position {
+    private fun moveSouth(): Position {
         if (y.isUnderBoundary()) y = SIZE - 1 else y--
         return Position(x, y, direction)
     }
 
-    fun moveEast(): Position {
+    private fun moveEast(): Position {
         if (x.isOverBoundary()) x = 0 else x++
         return Position(x, y, direction)
     }
 
-    fun moveWest(): Position {
+    private fun moveWest(): Position {
         if (x.isUnderBoundary()) x = SIZE - 1 else x--
         return Position(x, y, direction)
     }
